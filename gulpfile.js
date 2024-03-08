44fb7534c20dbb9493f47e5de6ec8db8d19b23f8
guigo <149022375+Guil029@users.noreply.github.com>
2024-03-08 18:19:24 -0300

    Criei uma variavel

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
+const gulp =  required('gulp');
+
 function funcaoPadrao(callback) {
     console.log("Executando via Gulp");
     callback();
 }
 
-exports.default = funcaoPadrao;
+function dizOi(callback) {
+    console.log("Olá Gulp");
+    diztchau();
+    callback();
+}
+
+function diztchau() {
+    console.log("Tchau Gulp");
+}
+
+exports.default = gulp.series(funcaoPadrao,);
+exports.dizOi = dizOi;

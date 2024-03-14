1efa407af09f3531c489ed435d95d66085be0603
guigo <149022375+Guil029@users.noreply.github.com>
2024-03-13 21:25:26 -0300

    ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp =  require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
@@ -37,3 +44,4 @@ exports.sass = compilaSass;
 exports.watch = function() {
     gulp.watch('./source/styles/*.scss', { ignoreInitial: false} , gulp.series(compilaSass));
 }
+exports.javascript = comprimeJavaScript;
